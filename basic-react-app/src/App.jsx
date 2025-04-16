fa0afc202108996c9c47e5b529dcb4c58a3380f9
Asmita Dahule <dahuleasmita@gmail.com>
2025-04-16 14:49:13 +0530

    some more components

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
 
+function Title() {
+  return <h1> I am the title! </h1>
+}
+
+function Description() {
+  return <h1> I am the description! </h1>
+}
+
 function App() {
   return (
    <div>
+     <Title/>
      <h1>Want to see magic !</h1>
+     <Title/>
      <button>click here</button>
+     <Title/>
+     <Description/>
    </div>
-  )
+  );
 }
 
 export default App
